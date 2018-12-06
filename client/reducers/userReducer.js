412e163d45ab87a6283a294c4d5fad23fcfe4dd2
Donte' Nall <code4zelda@gmail.com>
2018-12-05 21:34:54 -0800

    added new reducer to delete ordernumber once done btn is click

--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -16,7 +16,7 @@ const initialState = {
   logged: false
 };
 
-const userReducer = (state=initialState, action) => {
+const userReducer = (state = initialState, action) => {
 
   let rest_id;
   let rest_name;
@@ -31,7 +31,7 @@ const userReducer = (state=initialState, action) => {
   let rest_imagelink;
   let logged;
 
-  switch(action.type) {
+  switch (action.type) {
 
     case types.LOG_EMAIL:
       emailField = action.payload.toLowerCase();
@@ -47,9 +47,9 @@ const userReducer = (state=initialState, action) => {
         ...state,
         passwordField,
       };
-    
+
     case types.LOG_IN:
-      const { rest_id, rest_name, rest_email, rest_password, rest_address, rest_city, rest_state, rest_zipcode, rest_phone, rest_yelplink, rest_imagelink} = action.payload;
+      const { rest_id, rest_name, rest_email, rest_password, rest_address, rest_city, rest_state, rest_zipcode, rest_phone, rest_yelplink, rest_imagelink } = action.payload;
 
       id = rest_id;
       name = rest_name;
@@ -59,7 +59,7 @@ const userReducer = (state=initialState, action) => {
       city = rest_city;
       state = rest_state;
       zipcode = rest_zipcode;
-      phone = rest_phone; 
+      phone = rest_phone;
       yelplink = rest_yelplink;
       imagelink = rest_imagelink;
       logged = true;
@@ -78,44 +78,44 @@ const userReducer = (state=initialState, action) => {
         imagelink,
         logged,
       };
-      // fetch('http://192.168.86.234:3000/user/login', {
-      //   method: 'POST',
-      //   mode: 'cors',
-      //   headers: {
-      //     'Content-Type': 'application/json; charset=utf-8',
-      //     'Access-Control-Allow-Origin': '*',
-      //   },
-      //   body: JSON.stringify({
-      //     email: state.emailField,
-      //     password: state.passwordField,
-      //   }),
-      // })
-      //   .then(res => res.json())
-      //   .then((data) => {
-      //     id = data['user_id'];
-      //     firstName = data['user_firstname'];
-      //     lastName = data['user_lastname'];
-      //     email = data['user_email'];
-      //     phone = data['user_phone'];
-      //     emailField = '';
-      //     passwordField = '';
-      //     logged = true;
-      //     // console.log(state);
-      //     return {
-      //       ...state,
-      //       // id,
-      //       // firstName,
-      //       // lastName,
-      //       // email,
-      //       // phone,
-      //       // emailField,
-      //       // passwordField,
-      //       logged,
-      //     };
-      //   })
-      //   .catch((error) => {
-      //     console.log(error);
-      //   });
+    // fetch('http://192.168.86.234:3000/user/login', {
+    //   method: 'POST',
+    //   mode: 'cors',
+    //   headers: {
+    //     'Content-Type': 'application/json; charset=utf-8',
+    //     'Access-Control-Allow-Origin': '*',
+    //   },
+    //   body: JSON.stringify({
+    //     email: state.emailField,
+    //     password: state.passwordField,
+    //   }),
+    // })
+    //   .then(res => res.json())
+    //   .then((data) => {
+    //     id = data['user_id'];
+    //     firstName = data['user_firstname'];
+    //     lastName = data['user_lastname'];
+    //     email = data['user_email'];
+    //     phone = data['user_phone'];
+    //     emailField = '';
+    //     passwordField = '';
+    //     logged = true;
+    //     // console.log(state);
+    //     return {
+    //       ...state,
+    //       // id,
+    //       // firstName,
+    //       // lastName,
+    //       // email,
+    //       // phone,
+    //       // emailField,
+    //       // passwordField,
+    //       logged,
+    //     };
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
     case types.GET_ORDERS:
       console.log(action.payload);
       let orders = action.payload;
@@ -126,9 +126,32 @@ const userReducer = (state=initialState, action) => {
       };
 
     case types.COMPLETE_ORDER:
-      console.log('QWERTY REDUCER CALLED', action.payload);
+      console.log('COMPLETE ORDER REDUCER CALLED', action.payload);
+
+      let orderNum = action.payload.orderId * 1;
+      let ordersCopy = state.orders.slice();
+
+      ordersCopy = state.orders.filter(order => {
+        console.log('orderNum ', orderNum)
+        console.log('order ', order['fk_orders'])
+        return orderNum !== order['fk_orders']
+      })
+
+      const result = [];
+      for (let i = 0; i < ordersCopy.length; i += 1) {
+        const order = ordersCopy[i];
+        console.log('orderNum ', orderNum)
+        console.log('order ', order['fk_orders'])
+
+        if (orderNum !== order['fk_orders']) {
+          console.log('pushingggg ', order)
+          result.push(order);
+        }
+      }
+      console.log('changing state to ', result)
       return {
         ...state,
+        orders: result,
       };
 
     default:
